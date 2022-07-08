dc36dc56a8e4ed27733276207b115da80707cc5a
Oscar Vallejo <Oscar.Vallejo@Neoris.com>
2022-07-08 17:42:21 -0500

    orbe sonarcloud config.yml

--- src/test/java/com/lulobank/stepsdefinitions/CreateEmployeeStepDefinition.java
+++ src/test/java/com/lulobank/stepsdefinitions/CreateEmployeeStepDefinition.java
@@ -3,14 +3,10 @@ package com.lulobank.stepsdefinitions;
 import com.lulobank.integrations.DummyRestApi;
 import com.lulobank.questions.CheckResponse;
 import com.lulobank.tasks.RandomlyGenerated;
-import com.lulobank.utils.EmployeeData;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.actors.OnStage;
-import net.serenitybdd.screenplay.rest.questions.ResponseConsequence;
-
-import static org.hamcrest.Matchers.*;
 
 public class CreateEmployeeStepDefinition {
     @Given("Create Employee Random Data")
---
+++ src/test/java/com/lulobank/stepsdefinitions/DeleteEmployeeRecordStepDefinition.java
@@ -0,0 +1,15 @@
+
+package com.lulobank.stepsdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class DeleteEmployeeRecordStepDefinition {
+    @Given("Delete Employee Record")
+    public void deleteEmployeeRecord() {
+    }
+
+    @Then("Check Delete Record Request")
+    public void checkDeleteRecordRequest() {
+    }
+}
