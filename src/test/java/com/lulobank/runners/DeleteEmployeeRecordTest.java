01022e882d15abd3334da56cf9e87315419015d1
Oscar Vallejo <Oscar.Vallejo@Neoris.com>
2022-07-08 21:54:27 -0500

    circleci store_artifacts

--- src/test/java/com/lulobank/runners/DeleteEmployeeRecordTest.java
+++ src/test/java/com/lulobank/runners/DeleteEmployeeRecordTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/features/delete_employee_record.feature",
         glue = "com.lulobank.stepsdefinitions",
         plugin ={"pretty",
-                "junit:target/cucumber-reports/Cucumber.xml",
-                "html:target/cucumber-reports"}
+                "junit:target/cucumber-reports/cucumber.xml",
+                "html:target/cucumber-reports/report-html"}
 )
 public class DeleteEmployeeRecordTest {
 }
--- src/test/java/com/lulobank/runners/UpdateEmployeeDataTest.java
+++ src/test/java/com/lulobank/runners/UpdateEmployeeDataTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/features/update_employee_data.feature",
         glue = "com.lulobank.stepsdefinitions",
         plugin ={"pretty",
-                "junit:target/cucumber-reports/Cucumber.xml",
-                "html:target/cucumber-reports"}
+                "junit:target/cucumber-reports/cucumber.xml",
+                "html:target/cucumber-reports/report-html"}
 )
 public class UpdateEmployeeDataTest {
 }
